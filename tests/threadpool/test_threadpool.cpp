649b782007c17578927ffae8ecd6eb698084407e
happyAnger6 <happyAnger6@163.com>
2021-03-06 05:57:02 -0800

    feat:add threadpoool testcases

--- tests/threadpool/test_threadpool.cpp
+++ tests/threadpool/test_threadpool.cpp
@@ -40,3 +40,37 @@ TEST(threadpool, submit_one_work)
 
     EXPECT_EQ(val, 20);
 }
+
+TEST(threadpool, submit_three_work) 
+{
+    int val1 = 10, val2 = 20, val3 = 100;
+    struct threadpool* thpl = threadpool_create(5);
+    struct task one_task = {
+        .do_task = task_fn,
+        .args = (void *)&val1
+    };
+
+    struct task two_task = {
+        .do_task = task_fn,
+        .args = (void *)&val2
+    };
+
+    struct task three_task = {
+        .do_task = task_fn,
+        .args = (void *)&val3
+    };
+    
+    EXPECT_NE(thpl, nullptr);
+    EXPECT_EQ(thpl->threads_num, 0);
+    EXPECT_EQ(thpl->status, STATUS_RUNNING);
+
+    threadpool_submit(thpl, &one_task);
+    threadpool_submit(thpl, &two_task);
+    threadpool_submit(thpl, &three_task);
+    threadpool_shutdown(thpl, WAIT_ALL_DONE, NULL);
+    threadpool_wait(thpl);
+
+    EXPECT_EQ(val1, 20);
+    EXPECT_EQ(val2, 30);
+    EXPECT_EQ(val3, 110);
+}
