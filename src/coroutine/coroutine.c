cf4b031b8155bcdfab2976ab299efce13f6da207
happyAnager6 <happyAnger6@163.com>
2021-06-10 23:29:14 +0800

    feat: impl coroutine

--- src/coroutine/coroutine.c
+++ src/coroutine/coroutine.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <errno.h>
 #include <unistd.h>
 
+#include "list.h"
 #include "coroutine.h"
 
 static sched_t *main_sched;
@@ -17,59 +19,77 @@ static inline void error_exit(char *msg)
     exit(1);
 }
 
-void idle()
+static void co_wrapper()
 {
-    int i = 0;
-    while(g_run_flag){
-        sleep(1);
+    coroutine_t *co = main_sched->co_curr;
+    coroutine_func func = co->fn;
+    func(co->fn_data);
 
-        i++;
-        if(i == 10)
-        {
+    co_destory(co); // co already done.
+}
+
+int run_sched()
+{
+    return swapcontext(&main_sched->co_main->uctx, &main_sched->co_curr->uctx);
+}
+
+int select_one_co()
+{
+    coroutine_t *co;
+    if(list_empty(&main_sched->co_ready_list)) {
+        main_sched->co_curr = NULL; // no ready, go to main loop
+        return -1;
+    }
+
+    co = list_entry(main_sched->co_ready_list.next, coroutine_t, list);
+    list_del(&co->list);
+    main_sched->co_curr = co;
+    return 0;
+}
+
+void idle(void *args)
+{
+    while(g_run_flag){
+        if(select_one_co() == -1) {
             g_run_flag = 0;
+            puts("all coroutines done!\r\n");
             break;
         }
 
-        puts("idle");
-
-        if (swapcontext(&(main_sched->co_main->uctx), &uctx_main_loop) == -1)
-            error_exit("main sched switch to loop error");
-        
+        run_sched();
     }
 }
 
-static int _co_new(coroutine_func fn, void *args)
+static coroutine_t* _co_new(coroutine_func fn, void *args, int main_flag)
 {
-    coroutine_t co = (coroutine_t *)malloc(sizeof(coroutine_t));
+    coroutine_t *co = (coroutine_t *)malloc(sizeof(coroutine_t));
     if(co == NULL)
         error_exit("co new failed!");
 
     co->fn = fn;
     co->fn_data = args;
     co->status = CO_READY;
+    INIT_LIST_HEAD(&co->list);
 
-    co->uctx = (ucontext_t *)malloc(sizeof(ucontext_t));
-    if(co->uctx == NULL)
-        error_exit("co uctx malloc failed!");
-
-    if(getcontext(co->uctx) == -1)
+    if(getcontext(&co->uctx) == -1)
         error_exit("getcontext failed!");
 
-    co->uctx->uc_stack.ss_sp = malloc(CO_STACK_SIZE);
-    if(co->uctx->uc_stack.ss_sp == NULL)
+    co->uctx.uc_stack.ss_sp = malloc(CO_STACK_SIZE);
+    if(co->uctx.uc_stack.ss_sp == NULL)
         error_exit("co uctx malloc stack failed!");
-    co->uctx->uc_stack.ss_size = CO_STACK_SIZE;
-    co->uctx->uc_link = &main_sched->co_main->uctx; //if end, return to idle.
-    if(makecontext(co->uctx, fn, 1, co->fn_data) == -1)
-        error_exit("makecontext failed!");
+    co->uctx.uc_stack.ss_size = CO_STACK_SIZE;
+    co->uctx.uc_link = &uctx_main_loop; //if end, return to main_loop.
+    makecontext(&co->uctx, co_wrapper, 0);
 
-    return 0;
+    if(main_flag == 0)
+        list_add_tail(&co->list, &main_sched->co_ready_list);
+
+    return co;
 }
 
-
-int coroutine_create(coroutine_fn co_fn, void *args)
+coroutine_t* coroutine_create(coroutine_func co_fn, void *args)
 {
-    return _co_new(fn, args);
+    return _co_new(co_fn, args, 0);
 }
 
 int coroutine_init()
@@ -82,24 +102,12 @@ int coroutine_init()
     if(main_sched == NULL)
         error_exit("malloc main sched_t failed\r\n");
 
-    main_co = (coroutine_t *)malloc(sizeof(sched_t));
-    if(main_co == NULL)
-        error_exit("main_co malloc failed\r\n");
-
-    char *main_co_stack = (char *)malloc(CO_STACK_SIZE);
-    if(main_co_stack == NULL)
-        error_exit("main_co_stack malloc failed\r\n");
+    INIT_LIST_HEAD(&main_sched->co_ready_list);
     
-    if(getcontext(&main_co->uctx) == -1)
-        error_exit("malloc main sched_t failed\r\n");
-    ucontext_t *uctx_main_co = &main_co->uctx;
-    uctx_main_co->uc_stack.ss_sp = main_co_stack;
-    uctx_main_co->uc_stack.ss_size = CO_STACK_SIZE;
-    uctx_main_co->uc_link = &uctx_main_loop;
-    makecontext(uctx_main_co, idle, 0);
-
+    main_co = _co_new(idle, NULL, 1);
     main_sched->co_main = main_co;
     main_sched->co_curr = main_co;
+
     return 0;
 }
 
@@ -114,19 +122,43 @@ int coroutine_loop()
     return 0;
 }
 
-int coroutine_sched()
+int coroutine_yield()
 {
+    coroutine_t *co = main_sched->co_curr;
+    list_add_tail(&co->list, &main_sched->co_ready_list);  // resched later.
+
     if(swapcontext(&main_sched->co_curr->uctx, &main_sched->co_main->uctx)==-1)
         error_exit("sched swapcontext failed!");
     
     return 0;
 }
 
+void co_destory(coroutine_t *co)
+{
+    assert(co != NULL);
+}
+
 #ifdef TEST_MAIN
+
+void co1(void *args)
+{
+    puts("co1 begin...");
+    coroutine_yield();
+    puts("co1 end...");
+}
+
+void co2(void *args)
+{
+    puts("co2 begin...");
+    coroutine_yield();
+    puts("co2 end...");
+}
 
 int main()
 {
     coroutine_init();
+    coroutine_create(co1, NULL);
+    coroutine_create(co2, NULL);
 
     return coroutine_loop();
 }
