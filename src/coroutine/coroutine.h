178a3096f803d270ee97dba0bc268263e3769dce
happyAnager6 <happyAnger6@163.com>
2021-06-10 23:29:14 +0800

    feat: impl coroutine

--- src/coroutine/coroutine.h
+++ src/coroutine/coroutine.h
@@ -3,25 +3,22 @@
 
 #include <ucontext.h>
 
+#include "list.h"
+
 #define CO_STACK_SIZE 16*1024
 
 typedef void (*coroutine_func)(void*);
 
 typedef enum {
     CO_CREATE=0,
-<<<<<<< HEAD
     CO_READY,
     CO_RUNNINE,
     CO_SUSPEND,
     CO_END
-=======
-    CO_RUNNINE=1,
-    CO_SUSPEND=2,
-    CO_END=3
->>>>>>> 48895903c642875dfee71baa52a3e1cb3883909e
 }CO_STATUS;
 
 typedef struct {
+    struct list_head list;
     ucontext_t uctx;
     void *fn_data;
     coroutine_func fn;
@@ -29,13 +26,16 @@ typedef struct {
 }coroutine_t;
 
 typedef struct {
+    struct list_head co_ready_list;
     coroutine_t *co_main;
     coroutine_t *co_curr;
 }sched_t;
 
 
 extern int coroutine_init();
+extern coroutine_t* coroutine_create(coroutine_fn co_fn, void *args)
+extern int coroutine_yield();
 extern int coroutine_loop();
-extern int coroutine_sched();
+extern void void co_destory(coroutine_t *co);
 
 #endif
